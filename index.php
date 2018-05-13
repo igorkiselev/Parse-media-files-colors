63481671bff90c4d931d2e9d65055fd6400c0308
Ekaterina Fateeva <ekaterinafateeva@Ekaterinas-iMac.local>
2018-05-13 21:14:27 +0300

    sad

--- index.php
+++ index.php
@@ -15,184 +15,237 @@ if (! defined('ABSPATH')) {
 
 load_plugin_textdomain('media-colors', false, dirname(plugin_basename(__FILE__)) . '/languages/');
 
-include( 'colorsofimage.class.php' );
+include('colorsofimage.class.php');
 
 add_action('admin_init', function () {
     register_setting('media-colors', 'media-colors_settings');
 });
 
-add_filter( 'wp_handle_upload', function($upload, $context){
+add_filter('wp_handle_upload', function ($upload, $context) {
+}, 10, 2);
 
-}, 10, 2 );
 
-
-add_action('admin_head', function(){
+add_action('admin_head', function () {
     echo	'<style>'.
-			'span.color{width:2em; height:2em; margin-right:.5em; display:inline-block; border-radius:2px; border:1px Solid rgba(0,0,0,0.1)}'.
-			'span.marker{display:inline-block; padding:2px 8px; border-radius:2px;}'.
-				'span.marker.dark{background-color:#000; color:#fff}'.
-				'span.marker.light{background-color:#fff; color:#000}'.
-			'</style>';
+            'span.color{width:2em; height:2em; margin-right:.5em; margin-bottom:.5em; display:inline-block; border-radius:2px; border:1px Solid rgba(0,0,0,0.1)}'.
+                'span.marker{display:inline-block; padding:2px 8px; border-radius:2px;}'.
+                'span.marker.dark{background-color:#000; color:#fff}'.
+                'span.marker.light{background-color:#fff; color:#000}'.
+            '</style>';
 });
 
 $set = get_option('media-colors_settings');
 
-function attachment_palette($id){
-	
-	$array = get_post_meta( $id, 'media-color', true );
+function _attachment_palette($id)
+{
+    $array = get_post_meta($id, 'media-color', true);
 
-	if($array){
-			
-		$colors = "";
-			
-            foreach($array as &$value){
-				
-                $colors .= '<span class="color" style="background-color:'.$value.';"></span>';
-				
-            }
-			
-            return $colors;
-			
-			
+    if ($array) {
+        $colors = "";
+            
+        foreach ($array as &$value) {
+            $colors .= '<span class="color" style="background-color:'.$value.';"></span>';
         }
-		
-		
+            
+        return $colors;
+    }
+}
 
+function _get_all_attachments_count(){
+	
+    $query_images_args = array(
+        'post_type' => 'attachment', 'post_mime_type' =>'image', 'post_status' => 'inherit', 'posts_per_page' => -1,
+        'meta_query' => array(array('key'     => 'media-color','compare' => 'NOT EXISTS'),
+        ),
+    );
+
+    $query_images = new WP_Query($query_images_args);
+	
+	return count($query_images->posts);
 	
 }
 
-function attachment_hex_average($array){
-	
-    $tone = 0;
-	
-	foreach($array as &$value){
+function _get_all_attachments()
+{
+    global $set;
         
+    
+    $query_images_args = array(
+        'post_type' => 'attachment',
+        'post_mime_type' =>'image',
+        'post_status' => 'inherit',
+        'posts_per_page' => 60,
+        'meta_query' => array(
+            array(
+                'key'     => 'media-color',
+                'compare' => 'NOT EXISTS',
+            ),
+        ),
+    );
+
+    $query_images = new WP_Query($query_images_args);
+    
+    foreach ($query_images->posts as $image) {
+		
+        $src = wp_get_attachment_image_src($image->ID, $set['quality'])[0];
+        
+        $colors = _attachment_hex_array($src);
+        
+        $tone = (_attachment_hex_average($colors) > (255 / 2)) ? true : false;
+        
+        update_post_meta($image->ID, 'media-color', $colors);
+        
+        update_post_meta($image->ID, 'media-tone', $tone);
+
+    }
+}
+
+function _attachment_hex_average($array)
+{
+    $tone = 0;
+    
+    foreach ($array as &$value) {
         list($r, $g, $b) = sscanf($value, "#%02x%02x%02x");
         
         $tone += ($r + $g + $b) / 3;
     }
-	
+    
     return floor($tone / count($array));
 }
 
-function attachment_hex_array($src){
-	
-	$class = new ColorsOfImage( $src );
+function _attachment_hex_array($src)
+{
+    $class = new ColorsOfImage($src);
 
-	$colors = $class->getProminentColors();
-	
-	$background = $class->getBackgroundColor();
-	
-	if($background){ $colors[] = $background; }
-	
- 	return $colors;
+    $colors = $class->getProminentColors();
+    
+    $background = $class->getBackgroundColor();
+    
+    if ($background) {
+        $colors[] = $background;
+    }
+    
+    return $colors;
 }
 
-add_filter( 'attachment_fields_to_edit', function( $form_fields, $post ){
-	
+add_filter('attachment_fields_to_edit', function ($form_fields, $post) {
     global $set;
-	
-    $colors = attachment_palette($post->ID);
     
-    if($colors):
+    $colors = _attachment_palette($post->ID);
+    
+    if ($colors):
         
         $form_fields['media-color'] = array(
-			
+            
             'label' => __('Main colors in the image', 'media-colors'),
-			
+            
             'input' => 'html',
-			
-			'html' => $colors,
-			
+            
+            'html' => $colors,
+            
             'helps' => __('Colors that dominate in the image. Available for you as an array from post meta "media-color".', 'media-colors'),
-			
+            
         );
         
-        $tone = (get_post_meta( $post->ID, 'media-tone', true )) ? __('Light', 'media-colors') : __('Dark', 'media-colors');
+    $tone = (get_post_meta($post->ID, 'media-tone', true)) ? __('Light', 'media-colors') : __('Dark', 'media-colors');
         
-        $form_fields['media-tone'] = array(
-			
+    $form_fields['media-tone'] = array(
+            
             'label' => __('Image brightness', 'media-colors'),
-			
+            
             'input' => 'html',
-			
-			'html' => $tone,
-			
+            
+            'html' => $tone,
+            
             'helps' => __('Brightness of the image (dark or light). Available as a boolean from post meta "media-tone".', 'media-colors'),
-        );	
-	
+        );
+    
     endif;
 
     return $form_fields;
-    
-}, 10, 2 );
+}, 10, 2);
 
 
  
-add_filter( 'attachment_fields_to_save', function( $post, $attachment ){
-    
+add_filter('attachment_fields_to_save', function ($post, $attachment) {
     global $set;
         
-        $src = (!empty($set['quality'])) ? wp_get_attachment_image_src( $post->ID , $set['quality'])[0] : $post['attachment_url'];
+    $src = (!empty($set['quality'])) ? wp_get_attachment_image_src($post->ID, $set['quality'])[0] : $post['attachment_url'];
        
-        $colors = attachment_hex_array($src);
-    	
-        $tone = (attachment_hex_average($colors) > (255 / 2)) ? true : false;
+    $colors = _attachment_hex_array($src);
         
-        update_post_meta( $post['ID'], 'media-color', $colors );
+    $tone = (_attachment_hex_average($colors) > (255 / 2)) ? true : false;
         
-        update_post_meta( $post['ID'], 'media-tone', $tone);
+    update_post_meta($post['ID'], 'media-color', $colors);
+        
+    update_post_meta($post['ID'], 'media-tone', $tone);
         
     
     return $post;
-    
-}, 10, 2 );
+}, 10, 2);
 
-if(!empty($set['column'])){
-    add_filter( 'manage_media_columns', function($columns){
-    
-        $columns['colors'] = __( 'Colors', 'media-colors' );
-    	$columns['theme'] = __( 'Theme', 'media-colors' );
-		
+if (!empty($set['column'])) {
+    add_filter('manage_media_columns', function ($columns) {
+        $columns['colors'] = __('Colors', 'media-colors');
+        $columns['theme'] = __('Theme', 'media-colors');
+        
         return $columns;
-    
-    }, 10, 2 );
+    }, 10, 2);
 
 
-	add_action( 'manage_media_custom_column', function($column_name, $id){
-		switch ( $column_name ){
-			case 'colors' :
-				echo attachment_palette($id);
-			break;
-			case 'theme' :
-				$theme = get_post_meta( $id, 'media-tone');
-		 		
-				if(!empty($theme)):
-					echo ($theme[0]) ?  '<span class="marker light">'.__( 'Light', 'media-colors' ).'</span>' : '<span class="marker dark">'.__( 'Dark', 'media-colors' ).'</span>';
-				endif;
-			
-				break;
-			
-				default :
-				break;
-			}
-		}, 10, 2 );
+    add_action('manage_media_custom_column', function ($column_name, $id) {
+        switch ($column_name) {
+            case 'colors':
+                echo _attachment_palette($id);
+            break;
+            case 'theme':
+                $theme = get_post_meta($id, 'media-tone');
+                
+                if (!empty($theme)):
+                    echo ($theme[0]) ?  '<span class="marker light">'.__('Light', 'media-colors').'</span>' : '<span class="marker dark">'.__('Dark', 'media-colors').'</span>';
+                endif;
+            
+                break;
+            
+                default:
+                break;
+            }
+    }, 10, 2);
 }
+
+if (!empty($set['cron'])) {
+	
+    add_action('wp', function () {
+		
+        if (!wp_next_scheduled('mediacolorupdater')) {
+			
+            wp_schedule_event(time(), 'hourly', 'mediacolorupdater');
+        }
+    });
+	
+    register_deactivation_hook(__FILE__, function () {
+        wp_unschedule_event(wp_next_scheduled('mediacolorupdater'), 'mediacolorupdater');
+    });
+	
+    add_action('mediacolorupdater', '_get_all_attachments');
+}
+
 
 add_action('admin_menu', function () {
     add_options_page(
-    __("Media parser", "libraries"),
-    __("Media parser", "libraries"),
+    __('Media parser', 'media-colors'),
+    __('Media parser', 'media-colors'),
     'manage_options',
     'media-colors',
         function () {
-        if (!current_user_can('manage_options')) {
-            wp_die(__('You do not have sufficient permissions to access this page.', 'media-colors'));
-        }
+            if (!current_user_can('manage_options')) {
+                wp_die(__('You do not have sufficient permissions to access this page.', 'media-colors'));
+            }
 
         
-        include(plugin_dir_path(__FILE__) . 'options.php');
-    });
+            include(plugin_dir_path(__FILE__) . 'options.php');
+        }
+    );
 });
 
 add_filter('plugin_action_links_' . plugin_basename(__FILE__), function ($links) {
--- options.php
+++ options.php
@@ -20,10 +20,10 @@
 	<form method="post" action="options.php">
 	
 	<?php settings_fields('media-colors');
-	
-	$settings = get_option('media-colors_settings');
-	
-	?>
+    
+    $settings = get_option('media-colors_settings');
+    
+    ?>
 		
 	
 		<table class="form-table">
@@ -41,94 +41,14 @@
 			</tr>
 			<tr>
 				<th scope="row">
-<<<<<<< HEAD
 					<?php _e('Image size for parcing', 'media-colors'); ?>
-=======
-					&nbsp;
-				</th>
-				<td width="42%">
-					<?php _custom_checkbox('disable-attachment-pages', __('Attachment pages', 'libraries'), __('Remove attachment pages. If you fill all titles, captions and descriptions on the media file, then maybe keep the pages. Improves SEO of the website.', 'libraries')); ?>
-				</td>
-				<td width="42%">
-					
-				</td>
-			</tr>
-		</table>
-		<table class="form-table form-table-border">
-			<tr>
-				<th scope="row">
-					<?php _e('Wordpress interface', 'libraries'); ?>
-				</th>
-				<td width="42%">
-					<?php _custom_checkbox('disable-adminbar', __('Remove administrator bar', 'libraries'), __('Hide admin panel on the website', 'libraries')); ?>
-				</td>
-				<td width="42%">
-					<?php _custom_checkbox('enable-navmenus', __('Menu in nav', 'libraries'), __('Move the menu item to the main navigation bar', 'libraries')); ?>
-				</td>
-			</tr>
-			<tr>
-				<th scope="row">
-					
-				</th>
-				<td width="42%">
-					<?php _custom_checkbox('featured-admin-image', __('Feature images', 'libraries'), __('Add feature images in post and page lists', 'libraries')); ?>
-				</td>
-				<td width="42%">
-					<?php _custom_checkbox('custom-filetypes', __('Custom filetypes', 'libraries'), __('Add support for custom filetypes (SVG)', 'libraries')); ?>
-				</td>
-			</tr>
-			
-			
-		</table>
-		<table class="form-table form-table-border">
-			<tr>
-				<th scope="row">
-					<?php _e('Content parser', 'libraries'); ?>
-				</th>
-				<td width="42%">
-					<?php _custom_checkbox('content-the_title', __('No title', 'libraries'), __('Display the phrase "No title" in the_title when the title of the post or page is empty', 'libraries')); ?>
-				</td>
-				<td width="42%">
-					<?php _custom_checkbox('header-wp_title', __('Site name in title', 'libraries'), __('Display the name of the website (company) in the header after the page name (wp_title)', 'libraries')); ?>
-				</td>
-			</tr>
-			<tr>
-				<th scope="row">
-					&nbsp;
-				</th>
-				<td width="42%">
-					<?php _custom_checkbox('settings-privateprefix', __('Remove "private"', 'libraries'), __('Remove "private" prefix from posts in loop', 'libraries')); ?>
-				</td>
-				<td width="42%">
-					<?php _custom_checkbox('opengraph', __('Opengraph meta', 'libraries'), __('Opengraph meta fields in  &#60;head&#47;&#62;', 'libraries')); ?>
-				</td>
-			</tr>
-			<tr>
-				<th scope="row">
-					&nbsp;
-				</th>
-				
-				<td width="42%">
-					<?php _custom_checkbox('header-wp_title-separator', __('Change title separator', 'libraries'), __('Change the title separator', 'libraries')); ?><p><?php _custom_input('header-wp_title-separator-character', 'header-wp_title-separator', '', '', ''); ?></p>
-				</td>
-				<td width="42%">
-					<?php _custom_checkbox('media_oembed_filter', __('oEmbed contols', 'libraries'), __('Remove controls from youtube, mixcloud and vimeo interface', 'libraries')); ?>
-				</td>
-			</tr>
-			
-		</table>
-		<table class="form-table form-table-border">
-			<tr>
-				<th scope="row">
-					<?php _e('RSS feed', 'libraries'); ?>
->>>>>>> 4323dc12d7400c800d34ba91588d35435f3c543a
 				</th>
 				<td>
 					<select name='media-colors_settings[quality]'>
 						<?php foreach (get_intermediate_image_sizes() as $item) {
-								$selected = ($settings['quality']==$item) ? 'selected="selected"' : '';
-								echo '<option value="'.$item.'" '.$selected.'>'.$item.'</option>';
-						} ?>
+        					$selected = ($settings['quality']==$item) ? 'selected="selected"' : '';
+        					echo '<option value="'.$item.'" '.$selected.'>'.$item.'</option>';
+    					} ?>
 					</select>
 					<p class="description">
 						<?php _e('Select image parse quality (the smallest the fastest)', 'media-colors')?>
@@ -137,12 +57,19 @@
 			</tr>
 			<tr>
 				<th scope="row">
-					<?php _e('Debug', 'media-colors'); ?>
+					<?php _e('Basic settings', 'media-colors'); ?>
 				</th>
 				<td>
-					<label><input name="media-colors_settings[debug]" type="checkbox" value="1" <?php (isset($settings['debug']) ? checked('1', $settings['debug']) : false);?> /> <strong><?php _e('Show debugger information')?></strong></label>
+					<label><input name="media-colors_settings[cron]" type="checkbox" value="1" <?php (isset($settings['cron']) ? checked('1', $settings['cron']) : false);?> /> <strong><?php _e('Colors column', 'media-colors')?></strong></label>
+					<p class="description">
+						<?php
+						printf( esc_html__( 'You have %1$s, images that dont have colors attached. Add a cron task to convert 60 images every hour.', 'media-colors' ), _get_all_attachments_count() );
+						?>
+					</p>
+				
 				</td>
 			</tr>
+		
 		</table>
 		
 	<?php do_settings_sections('theme-options'); ?>
